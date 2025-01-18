53a2ab4e6d1c002a5c584946f223d13ce9537194
wootaiklee <fined000@naver.com>
2025-01-18 13:42:55 +0900

    fix: get users 수정

--- apps/edu/src/apis.ts
+++ apps/edu/src/apis.ts
@@ -27,6 +27,7 @@ export async function getCourseContents(
 export async function getUser(token: string): Promise<UserType | null> {
   try {
     const response = await fetch(`http://localhost:4000/users`, {
+      method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
       },
