ed779ac2c0a9c707041432cc128469fd5a29d237
wootaiklee <fined000@naver.com>
2025-02-05 22:08:11 +0900

    feat:test

--- apps/job/src/pages/page-list.tsx
+++ apps/job/src/pages/page-list.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import JobListContainer from "../containers/job-list-containers";
 
+//  PageList component
 const PageList: React.FC = () => {
   return <JobListContainer />;
 };
